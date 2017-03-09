f724b6661229a88131c05ab72199db2d70cb9d92
Craigieboy10 <craig-watt@live.co.uk>
2017-03-09 14:11:01 +0000

    recent Game Project files
    test

---
+++ Roll A Ball 2/Assets/Scripts/Ufo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ufo : Enemyship {
+
+    public void UFOEnemy() {
+        setShipName("UFO");
+    }
+}
